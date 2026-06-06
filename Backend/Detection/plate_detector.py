5017156d309bd8b297f2826488a055ca1e84e636
huutai02 <huutaiz246@gmail.com>
2026-06-06 22:45:47 +0700

    Xong khung plate detector

--- Backend/Detection/plate_detector.py
+++ Backend/Detection/plate_detector.py
@@ -7,5 +7,47 @@ Nhiệm vụ:
 - Trả về vị trí biển số
 """
 
-def detect_plate(image):
-    pass
+from typing import Dict, List
+
+
+class PlateDetector:
+
+    def __init__(self):
+        self.model = None
+
+    def load_model(self):
+        """
+        Sau này load YOLOv8 ở đây
+        """
+        pass
+
+    def detect_plate(self, image) -> List[Dict]:
+
+        # TODO:
+        # YOLO inference
+
+        return [
+            {
+                "bbox": [100, 150, 300, 220],
+                "confidence": 0.95
+            }
+        ]
+
+    def crop_plate(self, image, bbox):
+        """
+        Cắt vùng biển số
+        Gửi sang OCR
+        """
+
+        # TODO
+        return image
+
+    def process(self, image):
+
+        plates = self.detect_plate(image)
+
+        return {
+            "status": "success",
+            "total_plates": len(plates),
+            "plates": plates
+        }
